ec7f31cc72dafb7bf3739ec0986230b9ce5f04db
Alex Woodall <alexwoodall12@gmail.com>
2020-02-10 13:31:07 +1300

    Corrected end indices

--- Vertical GRF from IMU/visualise_data.py
+++ Vertical GRF from IMU/visualise_data.py
@@ -27,23 +27,39 @@ for i in range(len(data_files)):
 	cut_off = 10 # Running occurs in the 10 Hz to 30 Hz range
 	b, a = signal.butter(4, cut_off/(frequency/2), 'low')
 
-	a_filt = signal.filtfilt(b, a, a_y_ankle_l)
+	ay_filt = signal.filtfilt(b, a, a_y_ankle_l)
+	ax_filt = signal.filtfilt(b, a, a_x_ankle_l)
+	az_filt = signal.filtfilt(b, a, a_z_ankle_l)
 	R_filt = signal.filtfilt(b, a, R_ankle_l)
 
 	label = (np.array(read_file)[:,-1]).tolist()
 
+	standing_still_start = label.index('1')
+	standing_still_end = len(label) - 1 - label[::-1].index('1')
+
+	# Shift by average acceleration during standing still phase
+	gx = np.mean(ax_filt[standing_still_start:standing_still_end+1])
+	gy = np.mean(ay_filt[standing_still_start:standing_still_end+1])
+	gz = np.mean(az_filt[standing_still_start:standing_still_end+1])
+	gR = np.mean(R_filt[standing_still_start:standing_still_end+1])
+
+	ax_filt = ax_filt - gx
+	ay_filt = ay_filt - gy
+	az_filt = az_filt - gz
+	R_filt = R_filt - gR
+
+	# Each activity in the dataset has 3071 points
 	# Jogging = L10, running = L11, data is recorded at 50 Hz. 1 both jogging and running were performed for 1 minute each
 	jogging_start = label.index('10')
+	jogging_end = len(label) - 1 - label[::-1].index('10')
+
 	running_start = label.index('11')
-	jogging_end = running_start - 1
-	running_end = label.index('12') - 1
+	running_end = len(label) - 1 - label[::-1].index('11')
 
-	standing_still_start = label.index('1')
-	standing_still_end = label.index('2') - 1
-
-	#plt.plot(a_x_ankle_l,'r', label='x')
-	plt.plot(R_filt[running_start:running_end+1],'g', label='y')
-	#plt.plot(a_z_ankle_l,'b', label='z')
+	#plt.plot(ax_filt[running_start+1500:running_start+1601],'r', label='x')
+	plt.plot(ay_filt[running_start+1500:running_start+1601],'g', label='y')
+	#plt.plot(az_filt[running_start+1500:running_start+1601],'b', label='z')
+	#plt.plot(R_filt[running_start+1500:running_start+1601], 'k', label='resultant')
 	
 	plt.legend()
 	plt.show()
